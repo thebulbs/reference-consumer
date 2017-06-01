bdaf22687eb6d79a994b653ffce9518a81bd952e
Matthias Laug <matthias.laug@gmail.com>
2017-06-01 13:38:33 +0200

    using catch up consumer

--- src/consumer.js
+++ src/consumer.js
@@ -2,11 +2,15 @@ const client = require('node-eventstore-client')
 const knowledge = require('./adapter/knowledge')
 const config = require('./config')
 
-const eventAppeared = (subscription, event) => {
+const eventAppeared = (stream, event) => {
     knowledge.store(
         event.originalEvent.eventType,
         JSON.parse(event.originalEvent.data.toString())
     )
+}
+
+const liveProcessingStarted = () => {
+    console.log("Caught up with previously stored events. Listening for new events.")
 }
 
 const subscriptionDropped = (subscription, reason, error) =>
@@ -20,33 +24,26 @@ const settings = {
 const endpoint = config.eventstore.endpoint
 const connection = client.createConnection(settings, endpoint)
 
-connection.on('heartbeatInfo', heartbeatInfo => {
-    console.log('Connected to endpoint', heartbeatInfo.remoteEndPoint)
-    console.log('Heartbeat latency', heartbeatInfo.responseReceivedAt - heartbeatInfo.requestSentAt)
-})
+connection.connect().catch(err => console.log(err))
 
 connection.once("connected", tcpEndPoint => {
-    console.log(`Connected to eventstore at ${tcpEndPoint.host}:${tcpEndPoint.port}`)
-    connection.connectToPersistentSubscription(
-        'reference',
-        'bulb',
+    const subscription = connection.subscribeToStreamFrom(
+        "reference",
+        null,
+        true,
         eventAppeared,
+        liveProcessingStarted,
         subscriptionDropped,
         credentials
     )
+    console.log(`Connected to eventstore at ${tcpEndPoint.host}:${tcpEndPoint.port}`)
+    console.log(`subscription.isSubscribedToAll: ${subscription.isSubscribedToAll}`)
 })
 
-connection.on("error", error =>
-    console.log(`Error occurred on connection: ${error}`)
+connection.on("error", err =>
+    console.log(`Error occurred on connection: ${err}`)
 )
 
-connection.on("closed", reason => {
+connection.on("closed", reason =>
     console.log(`Connection closed, reason: ${reason}`)
-})
-
-
-connection.connect().catch((err) => {
-    console.log(err)
-})
-
-
+)
