7e5ebb2686f1deb945f8f177068d3d566766b2a9
Matthias Laug <matthias.laug@gmail.com>
2017-06-08 09:13:44 +0200

    fixed use of axios config

--- src/adapter/knowledge.js
+++ src/adapter/knowledge.js
@@ -4,7 +4,8 @@ const config = require('../config')
 module.exports = {
 
     store: (type, payload) => {
-        return axios.put({
+        return axios({
+            method: 'put',
             url: config.knowledge.url + '/' + payload.auth.user + '/bulbs/' + payload.data.bulb.uuid + "/references",
             data: payload.data.reference,
             headers: {
