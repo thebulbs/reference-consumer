1821026c9344b6a13348657229a928cd8b8736fd
Matthias Laug <matthias.laug@gmail.com>
2017-06-08 09:35:25 +0200

    adjusted axios usage for better testcase

--- src/adapter/knowledge.js
+++ src/adapter/knowledge.js
@@ -4,14 +4,14 @@ const config = require('../config')
 module.exports = {
 
     store: (type, payload) => {
-        return axios({
-            method: 'put',
-            url: config.knowledge.url + '/' + payload.auth.user + '/bulbs/' + payload.data.bulb.uuid + "/references",
-            data: payload.data.reference,
-            headers: {
-                Authorization: "Bearer: " + payload.auth.token
-            }
-        })
+        return axios.put(
+            config.knowledge.url + '/' + payload.auth.user + '/bulbs/' + payload.data.bulb.uuid + "/references",
+            {
+                data: payload.data.reference,
+                headers: {
+                    Authorization: "Bearer: " + payload.auth.token
+                }
+            })
     }
 
 }
--- test/knowledge-adapter-test.js
+++ test/knowledge-adapter-test.js
@@ -16,18 +16,18 @@ describe('Knowledge Adapter', function () {
         sandbox.restore()
     })
 
-    it('should PUT bulb to knowledge api', () => {
+    it('should PUT reference to knowledge api', () => {
         const axiosPutStub = sandbox.stub(axios, "put").callsFake(() => {
             return Promise.resolve()
         })
 
         let event = {
             data: {
-                reference : {
+                reference: {
                     reference: "asd",
                     uuid: "123-123-123"
                 },
-                bulb : {
+                bulb: {
                     summary: 'asdasd',
                     uuid: '9cc094c2-3e12-4b15-9d69-3cfe6240f6b7'
                 }
@@ -40,13 +40,15 @@ describe('Knowledge Adapter', function () {
 
         KnowledgeAdapter.store("addReference", event)
         sinon.assert.calledOnce(axiosPutStub)
-        sinon.assert.calledWith(axiosPutStub, {
-            url: config.knowledge.url + "/123/bulbs/" + event.data.bulb.uuid + "/references",
-            data: event.data.reference,
-            headers: {
-                Authorization: "Bearer: token"
+        sinon.assert.calledWith(axiosPutStub,
+            config.knowledge.url + "/123/bulbs/" + event.data.bulb.uuid + "/references",
+            {
+                data: event.data.reference,
+                headers: {
+                    Authorization: "Bearer: token"
+                }
             }
-        })
+        )
     })
 
 });
